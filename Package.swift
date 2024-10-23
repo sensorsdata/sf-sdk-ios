4c513ea2f57f9f5645b4d3b32addc55148252eda
Andrei Shender <andrei.s@canva.com>
2024-10-23 14:31:58 +1100

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,15 +8,25 @@ let package = Package(
         .iOS(.v8)
     ],
     products: [
-        .library(
-            name: "SensorsFocus",
-            targets: ["SensorsFocus"]),
+        .library(name: "SensorsFocus", targets: ["SensorsFocusDynamic"]),
+        .library(name: "SensorsFocusDynamicIDMDiagnosis", targets: ["SensorsFocusDynamicIDMDiagnosis"]),
+        .library(name: "SensorsFocusDynamicIDM", targets: ["SensorsFocusDynamicIDM"]),
+        .library(name: "SensorsFocusDynamic", targets: ["SensorsFocusDynamic"]),
+        .library(name: "SensorsFocusDynamicDiagnosis", targets: ["SensorsFocusDynamicDiagnosis"]),
+        .library(name: "SensorsFocusStaticIDMDiagnosis", targets: ["SensorsFocusStaticIDMDiagnosis"]),
+        .library(name: "SensorsFocusStaticIDM", targets: ["SensorsFocusStaticIDM"]),
+        .library(name: "SensorsFocusStatic", targets: ["SensorsFocusStatic"]),
+        .library(name: "SensorsFocusStaticDiagnosis", targets: ["SensorsFocusStaticDiagnosis"]),
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(
-            name: "SensorsFocus",
-            path: "SensorsFocus/SensorsFocus.xcframework"
-        )
+        .binaryTarget(name: "SensorsFocusDynamicIDMDiagnosis", path: "Dynamic+IDM+Diagnosis/SensorsFocus.xcframework"),
+        .binaryTarget(name: "SensorsFocusDynamicIDM", path: "Dynamic+IDM/SensorsFocus.xcframework"),
+        .binaryTarget(name: "SensorsFocusDynamic", path: "Dynamic/SensorsFocus.xcframework"),
+        .binaryTarget(name: "SensorsFocusDynamicDiagnosis", path: "DynamicDiagnosis/SensorsFocus.xcframework"),
+        .binaryTarget(name: "SensorsFocusStaticIDMDiagnosis", path: "Static+IDM+Diagnosis/SensorsFocus.xcframework"),
+        .binaryTarget(name: "SensorsFocusStaticIDM", path: "Static+IDM/SensorsFocus.xcframework"),
+        .binaryTarget(name: "SensorsFocusStatic", path: "Static/SensorsFocus.xcframework"),
+        .binaryTarget(name: "SensorsFocusStaticDiagnosis", path: "StaticDiagnosis/SensorsFocus.xcframework"),
     ]
 )
